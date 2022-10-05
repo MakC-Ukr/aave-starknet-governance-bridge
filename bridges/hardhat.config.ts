0192b56e6a3485169b4c1240060cc8125e516f2e
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-10-06 01:49:49 +0530

    todebug

--- bridges/hardhat.config.ts
+++ bridges/hardhat.config.ts
@@ -5,7 +5,7 @@ import "@shardlabs/starknet-hardhat-plugin";
 import "@nomiclabs/hardhat-ethers";
 
 dotenv.config();
-
+const { PRIVATE_KEY, ALCHEMY_KEY, HOSTNAME_L1, HOSTNAME_L2 } = process.env;
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -13,7 +13,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.10",
   starknet: {
     venv: "~/cairo_venv",
-    network: "devnet",
+    network: "l2_testnet",
     wallets: {
       OpenZeppelin: {
           accountName: "OpenZeppelin",
@@ -23,12 +23,16 @@ const config: HardhatUserConfig = {
     }
   },
   networks: {
-    devnet: {
-        url: "http://127.0.0.1:5050"
+    l2_testnet: {
+      url: `http://${HOSTNAME_L2 || "localhost"}:5050`,
     },
-    hardhat: {
-
-    }
+    l1_testnet: {
+      url: `http://${HOSTNAME_L1 || "127.0.0.1"}:8545`,
+    },
+    /* mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [PRIVATE_KEY],
+    }, */
   },
   mocha: {
     timeout: 1800000,
--- bridges/test/todebug.ts
+++ bridges/test/todebug.ts
@@ -5,16 +5,17 @@ import {
 } from "hardhat/types";
 
 describe("L1-L2 communication test", function () {
+    // const networkUrl: string = "localhost:8545/";
+    const networkUrl: string = (network.config as HttpNetworkConfig).url;
     
     it("should get network url", async () => {
-        console.log("network.config:", network.config);
-        const networkUrl: string = (network.config as HttpNetworkConfig).url;
         console.log("networkUrl: ", networkUrl);
         expect(networkUrl).not.to.be.undefined;
     });
 
-    // it("loadL1MessagingContract should work", async () => {
-    //     const { address: deployedTo, l1_provider: L1Provider } =
-    //         await starknet.devnet.loadL1MessagingContract(networkUrl);        
-    // });
+    it("loadL1MessagingContract should work", async () => {
+      // const networkUrl: string = (network.config as HttpNetworkConfig).url;
+        const { address: deployedTo, l1_provider: L1Provider } =
+            await starknet.devnet.loadL1MessagingContract(networkUrl);        
+    });
 });
